708a8cfd2ff50a8f9a27963ac9a33db1b493c110
kd-github-iitj <solanki.5@iitj.ac.in>
2025-09-17 13:28:57 +0530

    Completed

---
+++ .vercel/output/functions/api/index.func/index.mjs
@@ -0,0 +1,27 @@
+import serverless from "serverless-http";
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+const handleDemo = (req, res) => {
+  const response = {
+    message: "Hello from Express server"
+  };
+  res.status(200).json(response);
+};
+function createServer() {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.get("/api/ping", (_req, res) => {
+    const ping = process.env.PING_MESSAGE ?? "ping";
+    res.json({ message: ping });
+  });
+  app.get("/api/demo", handleDemo);
+  return app;
+}
+const index = serverless(createServer());
+export {
+  index as default
+};
+//# sourceMappingURL=index.mjs.map
